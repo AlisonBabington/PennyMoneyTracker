037825e27bf2d901959d53dea8ed1b11bad2d2de
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-08-24 12:22:52 +0100

    basic database structure created

---
+++ money_tracker/db/money_track.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE tags (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE merchants (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE accounts (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  balance REAL
+);
+
+CREATE TABLE transactions (
+  id SERIAL8 PRIMARY KEY,
+  description VARCHAR(255),
+  amount REAL,
+  tag_ID INT8 REFERENCES tags(id) ON DELETE CASCADE,
+  merchant_id INT8 REFERENCES merchants(id) ON DELETE CASCADE,
+  account_id INT8 REFERENCES accounts(id) ON DELETE CASCADE
+);
