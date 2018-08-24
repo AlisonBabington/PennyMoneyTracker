8bb899f927a45331483135e00af31cfeb9943652
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-08-24 15:24:41 +0100

    merchant crud created

--- money_tracker/db/seeds.rb
+++ money_tracker/db/seeds.rb
@@ -7,6 +7,7 @@ require('pry-byebug')
 
 
 Tag.delete_all()
+Merchant.delete_all()
 
 tag1 = Tag.new({"name" => "Groceries"})
 tag1.save()
@@ -14,7 +15,17 @@ tag1.save()
 tag2 = Tag.new({"name" => "Entertainment"})
 tag2.save()
 
+merchant1 = Merchant.new({"name" => "Waitrose"})
+merchant1.save()
+
+merchant2 = Merchant.new({"name" => "Dominion Cinema"})
+merchant2.save()
+
+merchant3 = Merchant.new({"name" => "Footlights"})
+merchant3.save()
+
 tags = Tag.all
+merchants = Merchant.all
 
 binding.pry
 nil
--- money_tracker/models/merchants.rb
+++ money_tracker/models/merchants.rb
@@ -10,16 +10,54 @@ class Merchant
   end
 
   def save()
-    sql = "INSERT INT0 merchants
+    sql = "INSERT INTO merchants
     (name) VALUES ($1)
-    RETURNING ID"
+    RETURNING id "
     values = [@name]
     result = SqlRunner.run(sql, values).first
     @id = result['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE merchants
+    SET name = $1
+    WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+   sql = "DELETE FROM merchants
+   WHERE id = $1"
+   values = [@id]
+   SqlRunner.run(sql, values)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM merchants
+    WHERE name = $1"
+    values = [name]
+    found_merchant = SqlRunner.run(sql, values)
+    result = Merchant.map_merchant(found_merchant)
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM merchants
+    WHERE id = $1"
+    values = [id]
+    found_merchant = SqlRunner.run(sql, values)
+    result = Merchant.map_merchant(found_merchant)
+  end
+
+  def self.map_merchant(merchant_info)
+   result = merchant_info.map {|merchant| Merchant.new(merchant)}
+   return result
+  end
+
   def self.all()
     sql = "SELECT * FROM merchants"
+    merchants = SqlRunner.run(sql)
+    result = Merchant.map_merchant(merchants)
   end
 
   def self.delete_all()
