c3a3825beb2a07206107cfebe785ce5c0b217ff3
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-08-26 13:46:48 +0100

    tags index page created, required model in app.rb  and controller methods added

--- money_tracker/app.rb
+++ money_tracker/app.rb
@@ -3,11 +3,12 @@ require( 'sinatra/contrib/all' )
 
 require_relative('./models/account')
 require_relative('./models/merchant')
+require_relative('./models/tag')
 also_reload( './models/*' )
 require_relative('controllers/account_controller')
 require_relative('controllers/merchant_controller')
 # require_relative('controllers/transaction_controller')
-# require_relative('controllers/tag_controller')
+require_relative('controllers/tag_controller')
 
 get '/' do
   erb( :index )
--- money_tracker/controllers/tag_controller.rb
+++ money_tracker/controllers/tag_controller.rb
@@ -1,2 +1,36 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+
+get '/tags' do # index
+  @tags = Tag.all()
+  erb( :"tags/index" )
+end
+
+
+get '/tags/:id' do # show
+  @tag = Tag.find_by_id(params[:id])
+  erb( :"tags/show" )
+end
+
+post '/tags' do # create
+  @tag = Tag.new(params)
+  @tag.save()
+  redirect to '/tags'
+end
+
+get '/tags/:id/edit' do #edit
+  @tag = Tag.find_by_id(params[:id])
+  erb(:"tags/edit")
+end
+
+post '/tags/:id' do #update
+  tag = Tag.new(params)
+  tag.update()
+  redirect tp "/tags/#{tag.id}"
+end
+
+post "/tags/:id/delete" do #delete
+  tag = Tag.find_by_id(params[:id])
+  tag.delete()
+  redirect to "/tags"
+end
