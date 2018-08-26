b3411a2daa6d84db7be73108e09edfeb5a7f6b4d
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-08-26 12:50:05 +0100

    merchant controller methods added

--- money_tracker/controllers/merchant_controller.rb
+++ money_tracker/controllers/merchant_controller.rb
@@ -5,3 +5,31 @@ get '/merchants' do # index
   @merchants = Merchant.all()
   erb( :"merchants/index" )
 end
+
+get '/merchants/:id' do # show
+  @merchant = Merchant.find_by_id(params[:id])
+  erb( :"merchants/show" )
+end
+
+post '/merchants' do # create
+  @merchant = Merchant.new(params)
+  @merchant.save()
+  redirect to '/merchants'
+end
+
+get 'merchants/:id/edit' do #edit
+  @merchant = Merchant.find_by_id(params[:id])
+  erb(:"merchants/edit")
+end
+
+post '/merchants/:id' do #update
+  merchant = Merchant.new(params)
+  merchant.update()
+  redirect tp "/merchants/#{merchant.id}"
+end
+
+post "/merchants/:id/delete" do #delete
+  merchant = Merchant.find_by_id(params[:id])
+  merchant.delete()
+  redirect to "/merchants"
+end
