c9d9a9e22ab33e1a320606fc53ae233dbdd8443e
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-08-26 14:22:17 +0100

    transactions show created

--- money_tracker/controllers/transaction_controller.rb
+++ money_tracker/controllers/transaction_controller.rb
@@ -1,7 +1,37 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 
+require_relative('../models/merchant')
+
 get '/transactions' do # index
   @transactions = Transaction.all()
   erb( :"transactions/index" )
+end
+
+get '/transactions/:id' do # show
+  @transaction = Transaction.find_by_id(params[:id])
+  erb( :"transactions/show" )
+end
+
+post '/transactions' do # create
+  @transaction = Transaction.new(params)
+  @transaction.save()
+  redirect to '/transactions'
+end
+
+get '/transactions/:id/edit' do #edit
+  @transaction = Transaction.find_by_id(params[:id])
+  erb(:"transactions/edit")
+end
+
+post '/transactions/:id' do #update
+  transaction = Transaction.new(params)
+  transaction.update()
+  redirect tp "/transactions/#{transaction.id}"
+end
+
+post "/transactions/:id/delete" do #delete
+  transaction = Transaction.find_by_id(params[:id])
+  transaction.delete()
+  redirect to "/transactions"
 end
