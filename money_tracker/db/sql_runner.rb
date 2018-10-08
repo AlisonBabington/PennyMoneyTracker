3d7867ed69fed09fbe813c0c08f15aa253562464
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-10-08 19:10:33 +0100

    typo fixed in sql runner

--- money_tracker/db/sql_runner.rb
+++ money_tracker/db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'd7vmdha24p2r8d', host: 'ec2-54-225-68-133.compute-1.amazonaws.com' })
+      db = PG.connect({ dbname: 'd7vmdha24p2r8d', host: 'ec2-54-225-68-133.compute-1.amazonaws.com'
       port: 5432, user: 'khgqxpfnhieonp', password: '241090e371da1cb7b0760d6796624b23a55fac104280ae1124d49bc48d3cedb8'})
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
